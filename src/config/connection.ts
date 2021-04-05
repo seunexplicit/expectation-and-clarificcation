5dc7c4bce87ab1adf58980fda50819a0e6856d34
seunexplicit <seunxplicit@gmail.com>
2021-04-05 19:15:40 +0100

    .

--- src/config/connection.ts
+++ src/config/connection.ts
@@ -4,8 +4,9 @@ import { ItemsModel } from '../../db/Item/item.schema';
 
 export async function CreateConnection(){
 
-	let sequelize = new Sequelize(process.env.DATABASE, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
-								  host: process.env.DATABASE_HOST,
+	let sequelize = new Sequelize('h032ac6z0fz3nixy','u9lo9b0ys8q7acji', 't8pe14tkw4li32tj', {
+								  host: "eyw6324oty5fsovx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+								  port:3306,
 								  dialect: 'mysql'
 								});
 
